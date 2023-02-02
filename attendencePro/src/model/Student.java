4ace92ce4e18310f984877bf0d7e0f4d95978f59
vandan2003 <124230648+vandan2003@users.noreply.github.com>
2023-02-02 11:03:28 +0530

    Add files via upload

--- attendencePro/src/model/Student.java
+++ attendencePro/src/model/Student.java
@@ -4,10 +4,26 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ftt
  */
 public class Student {
-    
+    private int id;
+    private String name;
+    private String batch;
+    private ArrayList AttendenceList;
+    private String qualification;
+    private String address;
+
+    public Student(int id, String name, String batch, ArrayList AttendenceList, String qualification, String address) {
+        this.id = id;
+        this.name = name;
+        this.batch = batch;
+        this.AttendenceList = AttendenceList;
+        this.qualification = qualification;
+        this.address = address;
+    }
 }
