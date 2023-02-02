4940dbc99fc15ac44a2c12da80a1db9a7aa034ed
vandan2003 <124230648+vandan2003@users.noreply.github.com>
2023-02-02 10:40:56 +0530

    Add files via upload

---
+++ attendencePro/src/attendencepro/AttendencePro.java
@@ -0,0 +1,19 @@
+package attendencepro;
+
+import model.Attendence;
+
+public class AttendencePro {
+
+    public static void main(String[] args) {
+        Attendence obj1 = new Attendence("101", "22/1/23", "10:00");
+        Attendence obj2 = new Attendence("102", "22/1/23", "10:00");
+        Attendence obj3 = new Attendence("103", "22/1/23", "10:00");
+        Attendence obj4 = new Attendence("104", "22/1/23", "10:00");
+        Attendence obj5 = new Attendence("105", "22/1/23", "10:00");
+        Attendence obj6 = new Attendence("106", "22/1/23", "10:00");
+        Attendence obj7 = new Attendence("107", "22/1/23", "10:00");
+        Attendence obj8 = new Attendence("108", "22/1/23", "10:00");
+
+    }
+
+}
---
+++ attendencePro/src/model/Attendence.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Attendence {
+    private String stdId;
+    private String date;
+    private String time;
+
+    public String getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(String stdId) {
+        this.stdId = stdId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Attendence(String stdId, String date, String time) {
+        this.stdId = stdId;
+        this.date = date;
+        this.time = time;
+    }
+}
