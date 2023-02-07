b916cd40dd4ccf6ab7766831ff04c3400973821a
vandan2003 <124230648+vandan2003@users.noreply.github.com>
2023-02-07 17:45:41 +0530

    Add files via upload

--- attendencePro/src/attendencepro/AttendencePro.java
+++ attendencePro/src/attendencepro/AttendencePro.java
@@ -1,7 +1,84 @@
 package attendencepro;
 
+import dao.AttendenceDao;
+import dao.StudentDao;
+import java.util.ArrayList;
+import model.*;
+
 public class AttendencePro {
     public static void main(String[] args) {
-    
+       ArrayList total = null;
+       ArrayList<AttendenceMain> attendenceMains;
+       
+       ArrayList<Student> students = StudentDao.getAllStudents();
+       String date = "05/02/2023";
+       for(Student st : students){
+          ArrayList<Attendence> al = AttendenceDao.getAttendenceByDateAndId(st.getId(), date);
+          String id = ""+st.getId();
+          String inTime;
+          String outTime;
+          String duration;
+          String totalBreakTime;
+          String status;
+          
+          if(al.size() == 0){
+              status = "Absent";
+              inTime = "--:--";
+              outTime = "--:--";
+              totalBreakTime = "--:--";
+              duration = "--:--";
+          }
+          else if(!TimeManager.checkHalfDay(TimeManager.getTotalInTime(al))){
+              status = "HALF-DAY";
+              inTime = al.get(0).getTime();
+              outTime = al.get(al.size()-1).getTime();
+              totalBreakTime = TimeManager.getTotalBreakTime(al);
+              duration = TimeManager.getTotalInTime(al);
+          }
+          else{
+              status = "Present";
+              inTime = al.get(0).getTime();
+              outTime = al.get(al.size()-1).getTime();
+              totalBreakTime = TimeManager.getTotalBreakTime(al);
+              duration = TimeManager.getTotalInTime(al);
+          }
+          
+          AttendenceMain am = new AttendenceMain(id, inTime, outTime, duration, totalBreakTime, status);
+        
+           System.out.println(am.getStdId()+" "+am.getInTime()+" "+am.getOutTime()+" "+am.getDuration()+" "+am.getTotalBreakTime()+" "+ am.getStatus());
+       }
+       
+//        ArrayList<Attendence> al = AttendenceDao.getAttendenceByDateAndId(35, "05/02/2023");
+//          String id = ""+35;
+//          String inTime;
+//          String outTime;
+//          String duration;
+//          String totalBreakTime;
+//          String status;
+//          
+//          if(al.size() == 0){
+//              status = "Absent";
+//              inTime = "--:--";
+//              outTime = "--:--";
+//              totalBreakTime = "--:--";
+//              duration = "--:--";
+//          }
+//          else if(!TimeManager.checkHalfDay(TimeManager.getTotalInTime(al))){
+//              status = "HALF-DAY";
+//              inTime = al.get(0).getTime();
+//              outTime = al.get(al.size()-1).getTime();
+//              totalBreakTime = TimeManager.getTotalBreakTime(al);
+//              duration = TimeManager.getTotalInTime(al);
+//          }
+//          else{
+//              status = "Present";
+//              inTime = al.get(0).getTime();
+//              outTime = al.get(al.size()-1).getTime();
+//              totalBreakTime = TimeManager.getTotalBreakTime(al);
+//              duration = TimeManager.getTotalInTime(al);
+//          }
+//          
+//          AttendenceMain am = new AttendenceMain(id, inTime, outTime, duration, totalBreakTime, status);
+//          System.out.println(am.getStdId()+" "+am.getInTime()+" "+am.getOutTime()+" "+am.getDuration()+" "+am.getTotalBreakTime()+" "+ am.getStatus());
     }
 }
--- attendencePro/src/attendencepro/TimeManager.java
+++ attendencePro/src/attendencepro/TimeManager.java
@@ -12,101 +12,95 @@ import model.Attendence;
  * @author ftt
  */
 public class TimeManager {
-     public static String getTotalBreakTime(ArrayList<Attendence> al){
-      String totalBreakTime = "00:00";
-        for(int i=1;i<al.size()-1;i++){
-            String dif = getDifferenceBtwTimes(al.get(i).getTime(), al.get(i+1).getTime());
+
+    public static String getTotalBreakTime(ArrayList<Attendence> al) {
+        String totalBreakTime = "00:00";
+        for (int i = 1; i < al.size() - 1; i++) {
+            String dif = getDifferenceBtwTimes(al.get(i).getTime(), al.get(i + 1).getTime());
             totalBreakTime = getAdditionBtwTimes(totalBreakTime, dif);
+//            System.out.println(totalBreakTime + "*"+dif);
+
             i++;
         }
-       return totalBreakTime;
+        return totalBreakTime;
     }
-    
-    public static String getTotalInTime(ArrayList<Attendence> al){
-       String totalTime = getDifferenceBtwTimes(al.get(0).getTime(), al.get(al.size()-1).getTime());
+
+    public static String getTotalInTime(ArrayList<Attendence> al) {
+        String totalTime = getDifferenceBtwTimes(al.get(0).getTime(), al.get(al.size() - 1).getTime());
         String totalBreakTime = getTotalBreakTime(al);
         totalTime = getDifferenceBtwTimes(totalBreakTime, totalTime);
-       return totalTime;
+        return totalTime;
     }
-    
-    public static String getDifferenceBtwTimes(String time1,String time2){
-      String arr1[] = time1.split(":");
-        String arr2[] = time2.split(":");
 
-        int entryHour = Integer.parseInt(arr1[0]);
-        int exitHour = Integer.parseInt(arr2[0]);
-        
-        int entryMin = Integer.parseInt(arr1[1]);
-        int exitMin = Integer.parseInt(arr2[1]);
-        
-        if(exitMin < entryMin){
-           exitMin=exitMin+60;
-           exitHour--;
-        }
-        
-        int totalMin = exitMin - entryMin;
-        
-
-        int totalHour = exitHour - entryHour;
-        
-        if(totalMin >= 60){
-           totalMin = totalMin-60;
-           totalHour++;
-        }
-      return totalHour+":"+totalMin;
-    }
-    
-    public static String getAdditionBtwTimes(String time1,String time2){
+    public static String getDifferenceBtwTimes(String time1, String time2) {
         String arr1[] = time1.split(":");
         String arr2[] = time2.split(":");
 
         int entryHour = Integer.parseInt(arr1[0]);
         int exitHour = Integer.parseInt(arr2[0]);
-        
+
         int entryMin = Integer.parseInt(arr1[1]);
         int exitMin = Integer.parseInt(arr2[1]);
-        
-        if(exitMin < entryMin){
-           exitMin=exitMin+60;
-           exitHour--;
+
+        if (exitMin < entryMin) {
+            exitMin = exitMin + 60;
+            exitHour--;
         }
-        
-        int totalMin = exitMin + entryMin;
-        
+
+        int totalMin = exitMin - entryMin;
 
         int totalHour = exitHour - entryHour;
-        
-        if(totalMin >= 60){
-           totalMin = totalMin-60;
-           totalHour++;
+
+        if (totalMin >= 60) {
+            totalMin = totalMin - 60;
+            totalHour++;
         }
-      return totalHour+":"+totalMin;
+        return totalHour + ":" + totalMin;
     }
-    
-    public static boolean checkHalfDay(){
-        String totalBreakTime = "7:20";
+
+    public static String getAdditionBtwTimes(String time1, String time2) {
+        String arr1[] = time1.split(":");
+        String arr2[] = time2.split(":");
+
+        int entryHour = Integer.parseInt(arr1[0]);
+        int exitHour = Integer.parseInt(arr2[0]);
+
+        int entryMin = Integer.parseInt(arr1[1]);
+        int exitMin = Integer.parseInt(arr2[1]);
+
+
+        int totalMin = exitMin + entryMin;
+
+        int totalHour = exitHour - entryHour;
+
+        if (totalMin >= 60) {
+            totalMin = totalMin - 60;
+            totalHour++;
+        }
+        return totalHour + ":" + totalMin;
+    }
+
+    public static boolean checkHalfDay(String totalTime) {
+//        String totalTime = "7:20";
         String marginTime = "7:15";
-        
-        String arr1[] = totalBreakTime.split(":");
+
+        String arr1[] = totalTime.split(":");
         String arr2[] = marginTime.split(":");
 
         int tbtHour = Integer.parseInt(arr1[0]);
-        int exitHour = Integer.parseInt(arr2[0]);
-        
-        int tbtMin = Integer.parseInt(arr1[1]);
-        int exitMin = Integer.parseInt(arr2[1]);
-        
-        if(exitMin < tbtMin){
-           exitMin=exitMin+60;
-           exitHour--;
-        }
-        
-        int totalMin = exitMin + tbtMin;
-        
+        int mHour = Integer.parseInt(arr2[0]);
 
-        int totalHour = exitHour - tbtHour;
-        
-        
-      return true;
+        int tbtMin = Integer.parseInt(arr1[1]);
+        int mMin = Integer.parseInt(arr2[1]);
+
+        if (tbtHour < mHour) {
+            return false;
+        } else if (tbtHour > mHour) {
+            return true;
+        } else if (tbtHour == mHour && tbtMin >= mMin) {
+            return true;
+        }
+
+        return false;
     }
 }
